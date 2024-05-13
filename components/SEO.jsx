5000f306c7829b4ad7b75e44839426cdaf8bc193
mohit9889 <raj.mohit198@gmail.com>
2024-05-13 20:56:33 +0530

    keyword added

--- components/SEO.jsx
+++ components/SEO.jsx
@@ -1,15 +1,16 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 
-const SEO = ({ title, description, ogImage }) => {
+const SEO = ({ title, description, ogImage, keywords }) => {
   const router = useRouter();
-  const ogUrl = process.env.NEXT_PUBLIC_BASE_URL + router.asPath;
+  const ogUrl = process.env.BASE_URL + router.asPath;
 
   return (
     <Head>
       {/* Basic Meta Tags */}
       <title>{title}</title>
       <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
 
       {/* Open Graph Meta Tags */}
       <meta property="og:title" content={title} />
